265206b8a91e162450f98f0679fed5d8c711ff7f
andrii <a.shpak.st@gmail.com>
2024-05-17 12:27:31 +0300

    design

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,53 @@
+const CACHE_NAME = 'hello-world-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icon.png',
+  './qr1.png',
+  './qr2.png',
+  './qr3.png',
+  './404.html'
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './icon.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./404.html');
+          }
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
